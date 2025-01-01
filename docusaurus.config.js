76678017e1937baaab03b0f2a38f6e422ce90c9c
Soulfur <notsoulfur@gmail.com>
2025-01-01 14:44:51 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -59,7 +59,7 @@ const config = {
           items: [
             {
               label: 'Get Started',
-              to: '/docs/intro',
+              to: '/intro',
             },
           ],
         },
